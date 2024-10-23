bcbbd044b42d0b3793af32cc6f459e5a3967cc89
silvermk1 <perezvelasco.miguel@gmail.com>
2024-10-23 17:29:46 +0200

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4") // Añade ConstraintLayout correctamente
+    implementation(libs.androidx.constraintlayout) // Añade ConstraintLayout correctamente
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
