e344e1183a2cb09c9917e2e3a50313095076e1dc
silvermk1 <perezvelasco.miguel@gmail.com>
2024-10-23 17:31:55 +0200

    Prueba Coonstrant arreglado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.constraintlayout) // Añade ConstraintLayout correctamente
+    implementation(libs.androidx.constraintlayout) // Prueba modificación
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
